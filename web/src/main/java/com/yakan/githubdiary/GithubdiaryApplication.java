805fb1c105f78650df417202340a0c1c9495e506
Kanji Miyatani <kanji09012458@gmail.com>
2023-11-28 00:31:33 +0900

    Create project

---
+++ web/src/main/java/com/yakan/githubdiary/GithubdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.yakan.githubdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubdiaryApplication.class, args);
+	}
+
+}
